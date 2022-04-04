67cc670b738b242589792812b66b027e2fee3815
Philipp Strauß <phil13.strauss@gmail.com>
2022-04-04 10:42:55 +0200

    added random factor & function to produce message

--- random.js
+++ random.js
@@ -1,8 +1,32 @@
-console.log('Hello');
 
-
+// create the arrays
 const a = ['you', 'they', 'she', 'he', 'we'];
 const b = ['can dance,', 'can praise the sun,', 'can take the hobbits to isengard,', 'can call gondor for aid,'];
 const c = ['if fate wills it so.', 'since it is for the best.', 'for hope never dies.', '... or just leave it be, who am i to judge.'];
 
-console.log(a[2], b[2], c[0]);
+
+// create the random factor
+function rdmA() {
+    min = 0;
+    max = a.length;
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  function rdmB() {
+    min = 0;
+    max = b.length;
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  function rdmC() {
+    min = 0;
+    max = c.length;
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+  
+// apply random to array a
+
+function rdmMessage(){
+    console.log(a[rdmA()], b[rdmB()], c[rdmC()])
+};
+rdmMessage();
